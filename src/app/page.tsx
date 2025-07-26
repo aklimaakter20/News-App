41243db02e7e2bb1c6d38b9c7bb2e551c21d3095
aklimaakter20 <aklimaakter20@gmail.com>
2025-07-26 16:00:29 +0600

    Navbar created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    <h1>hello developer</h1>
-    <Button>click</Button>
+    
    </div>
   );
 }
---
+++ src/components/Shared/Navbar.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import React from "react";
+import { Switch } from "@/components/ui/switch";
+import { IoMenuSharp } from "react-icons/io5";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "../ui/navigation-menu";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <header className="py-4 shadow-md">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+        <div className="text-xl font-bold">
+          <Link href="/" className="hover:text-green-500">
+            News-app
+          </Link>
+        </div>
+        {/* desktop-menu */}
+
+        <NavigationMenu className="hidden md:flex">
+          <NavigationMenuList>
+            <NavigationMenuItem className="flex items-center space-x-8">
+              <NavigationMenuLink href="/news" className="hover:text-green-500">
+                News
+              </NavigationMenuLink>
+
+              <NavigationMenuLink
+                href="/services"
+                className="hover:text-green-500"
+              >
+                Services
+              </NavigationMenuLink>
+
+              <NavigationMenuLink
+                href="/about"
+                className="hover:text-green-500"
+              >
+                About
+              </NavigationMenuLink>
+              <NavigationMenuLink
+                href="/contact "
+                className="hover:text-green-500"
+              >
+                Contact
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        {/* color switcher and login button */}
+        <div className="hidden md:flex items-center space-x-4">
+          <div className="flex items-center">
+            <Switch className="mr-2" />
+            <span className="mr-3">Dark Mode</span>
+
+            <Button variant="default">Login</Button>
+          </div>
+        </div>
+
+        {/* for mobile menu */}
+
+        <div className="md:hidden">
+            <Button variant="outline"><IoMenuSharp size={24}/></Button>
+        
+
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
